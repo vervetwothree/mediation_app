c5756291c2517eeaa28327c42140546cca4abe9d
vervetwothree <jeepon35@gmail.com>
2022-10-11 08:18:08 -0400

    Initial Commit

---
+++ meditation3/meditation3App.swift
@@ -0,0 +1,17 @@
+//
+//  meditation3App.swift
+//  meditation3
+//
+//  Created by Sean Hartigan on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct meditation3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
